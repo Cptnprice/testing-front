e35e3706b11e4a690497208ff0defe2252a73aa9
shalva <shalvachkuaselidze77@gmail.com>
2020-12-25 18:14:16 +0400

    catch incorrect characters

--- typing-speed-test-project/js/home.js
+++ typing-speed-test-project/js/home.js
@@ -4,6 +4,7 @@ let typedWords = 0;
 let i = 0;
 let testing = words[i].length;
 let testing2 = 0;
+let testing3 = false;
 let first = document.getElementById(i);
 first.classList.add("underline-word");
 
@@ -11,13 +12,27 @@ let typedText = document.getElementById("typed-text");
 
 typedText.addEventListener("keyup", (e) => {
     testing = words[i].length;
+    console.log(e.key, testing, testing2);
     if (testing2 < testing) {
         if (e.key == words[i][testing2]) {
             if (typedText.value == words[i].substring(0, testing2+1)) {
                 temp = document.getElementById(i);
-                console.log(temp.innerHTML);
-                temp.innerHTML = temp.innerHTML.substring(0, temp.innerHTML.includes("</span>") ? (temp.innerHTML.lastIndexOf("</span>")+7) : -1) + `<span class="typed-character">${words[i][testing2]}</span>` + words[i].substring(testing2+1);
+                if (testing3) {
+                    temp.innerHTML = temp.innerHTML.replace('class="incorrect-character', 'class="typed-character"');
+                    testing3 = false;
+                }
+                else{
+                    temp.innerHTML = temp.innerHTML.substring(0, temp.innerHTML.includes("</span>") ? (temp.innerHTML.lastIndexOf("</span>")+7) : -1) + `<span class="typed-character">${words[i][testing2]}</span>` + words[i].substring(testing2+1);
+                }
+                // temp = document.getElementById(i);
+                // console.log(temp.innerHTML);
                 testing2++;
+            }
+        }
+        else {
+            if (!testing3) {
+                temp.innerHTML = temp.innerHTML.substring(0, temp.innerHTML.includes("</span>") ? (temp.innerHTML.lastIndexOf("</span>")+7) : -1) + `<span class="incorrect-character">${words[i][testing2]}</span>` + words[i].substring(testing2+1);
+                testing3 = true;
             }
         }
     }
@@ -28,5 +43,6 @@ typedText.addEventListener("keyup", (e) => {
         document.getElementById(i).classList.add("underline-word");
         testing2 = 0;
         typedText.value = "";
+        testing3 = false;
     }
 })
