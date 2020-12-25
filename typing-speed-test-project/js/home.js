e8f64adcdefef027f87aa03f6d51bb7a0ad31e0c
shalva <shalvachkuaselidze77@gmail.com>
2020-12-25 16:19:27 +0400

    fixed not to count as correct when word is not already matching the word to be typed

--- typing-speed-test-project/js/home.js
+++ typing-speed-test-project/js/home.js
@@ -13,9 +13,12 @@ typedText.addEventListener("keyup", (e) => {
     testing = words[i].length;
     if (testing2 < testing) {
         if (e.key == words[i][testing2]) {
-            temp = document.getElementById(i);
-            temp.innerHTML = temp.innerHTML.substring(0, temp.innerHTML.includes("</span>") ? (temp.innerHTML.lastIndexOf("</span>")+7) : -1) + `<span class="typed-character">${words[i][testing2]}</span>` + words[i].substring(testing2+1);
-            testing2++;
+            if (typedText.value == words[i].substring(0, testing2+1)) {
+                temp = document.getElementById(i);
+                console.log(temp.innerHTML);
+                temp.innerHTML = temp.innerHTML.substring(0, temp.innerHTML.includes("</span>") ? (temp.innerHTML.lastIndexOf("</span>")+7) : -1) + `<span class="typed-character">${words[i][testing2]}</span>` + words[i].substring(testing2+1);
+                testing2++;
+            }
         }
     }
     else {
