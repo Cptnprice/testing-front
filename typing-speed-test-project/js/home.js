3c904519c146af14318ae77070316784536219b1
shalva <shalvachkuaselidze77@gmail.com>
2020-12-28 16:42:08 +0400

    words per minute

--- typing-speed-test-project/js/home.js
+++ typing-speed-test-project/js/home.js
@@ -1,7 +1,18 @@
 let textToType = "When writing client-side JavaScript for web sites or applications, you won't go very far before you start to use APIs";
 let words = textToType.split(" ");
 let typedWords = 0;
+let wordsPerMinuteContainer = document.getElementById("words-per-minute");
 let i = 0;
+let totalTypedCharacters = 0;
+let wordsPerMinuteTimerId;
+let time = document.getElementById("time");
+let wordsPerMinute = 0;
+let stopwatchStarted = false;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let totalTime = 0;
+let timerId;
 let testing = words[i].length;
 let testing2 = 0;
 let incorrectTyped = false;
@@ -10,12 +21,51 @@ first.classList.add("underline-word");
 
 let typedText = document.getElementById("typed-text");
 
+function increment() {
+    totalTime++;
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+
+    time.innerHTML = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" +
+                      (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" +
+                      (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00");
+
+    timer();
+}
+
+function timer() {
+    timerId = setTimeout(increment, 1000);
+}
+
+function setWordsPerMinute() {
+    wordsPerMinute = (totalTypedCharacters/5)/(totalTime/60);
+    wordsPerMinuteContainer.innerHTML = `WPM: ${wordsPerMinute}`;
+    wordsPerMinuteTimer();
+}
+
+function wordsPerMinuteTimer() {
+    wordsPerMinuteTimerId = setTimeout(setWordsPerMinute, 3000);
+}
+
 typedText.addEventListener("input", (e) => {
+    if (!stopwatchStarted) {
+        timer();
+        wordsPerMinuteTimer();
+        stopwatchStarted = true;
+    }
     testing = words[i].length;
     console.log(e.data, testing, testing2);
     if (testing2 < testing) {
         if (e.data == words[i][testing2]) {
             if (typedText.value == words[i].substring(0, testing2+1)) {
+                totalTypedCharacters++;
                 temp = document.getElementById(i);
                 if (incorrectTyped) {
                     temp.innerHTML = temp.innerHTML.replace('class="incorrect-character', 'class="typed-character"');
@@ -41,7 +91,11 @@ typedText.addEventListener("input", (e) => {
         document.getElementById(i).classList.remove("underline-word");
         typedWords++;
         if (typedWords == words.length) {
-            document.getElementById("test").classList.add("completed");
+            let result = document.getElementById("test");
+            result.classList.add("completed");
+            result.innerHTML = `Your WPM is ${wordsPerMinute}`;
+            clearTimeout(timerId);
+            clearTimeout(wordsPerMinuteTimerId);
         }
         i++;
         document.getElementById(i).classList.add("underline-word");
