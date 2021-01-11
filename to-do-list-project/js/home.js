9a035213f634c01b43193d0f1510de46b3bc7afe
shalva <shalvachkuaselidze77@gmail.com>
2021-01-11 22:27:41 +0400

    saving on submit button

--- to-do-list-project/js/home.js
+++ to-do-list-project/js/home.js
@@ -1,22 +1,31 @@
 let storage = window.localStorage;
 let toDoList = document.getElementById("notes-list");
+let submitButton = document.getElementById("submit-note");
+let titleInput = document.getElementById("note-title");
+let titles = JSON.parse(storage.getItem("titles")) ? JSON.parse(storage.getItem("titles")) : [];
+let addedNew = false;
 
-if (!storage.length) {
-    storage.setItem("titles", JSON.stringify(["one", "two", "three"]));
+function createOrUpdateList() {
+    if (!storage.length || addedNew) {
+        storage.setItem("titles", JSON.stringify(titles));
+        addedNew = false;
+    }
 }
 
 function createToDoList() {
-    let titles = JSON.parse(storage.getItem("titles"));
-    titles.forEach((title) => {
-        createToDo();
-    })
+    createOrUpdateList();
+    if (titles) {
+        titles.forEach((title) => {
+            createToDo(title);
+        })
+    }
 }
 
-function createToDo() {
+function createToDo(newTitle) {
     let eachToDo = document.createElement('div');
     eachToDo.classList.add("each-to-do");
     let title = document.createElement("li");
-    title.innerHTML = "testing";
+    title.innerHTML = newTitle;
     let checkButton = document.createElement("button");
     checkButton.classList.add("check-to-do");
     checkButton.innerHTML = '<i class="far fa-check-square"></i>';
@@ -28,5 +37,19 @@ function createToDo() {
     eachToDo.appendChild(deleteButton);
     toDoList.appendChild(eachToDo);
 }
+
+function saveToDo(newTitle) {
+    createToDo(newTitle);
+    titleInput.value = "";
+    titles.push(newTitle);
+    addedNew = true;
+    createOrUpdateList();
+}
+
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let newTitle = titleInput.value;
+    saveToDo(newTitle);
+})
 
 window.onload = createToDoList;
