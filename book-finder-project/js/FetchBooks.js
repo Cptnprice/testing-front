a28804a08440b96d7c87943a13d0b0a2b479fde1
shalva <shalvachkuaselidze77@gmail.com>
2021-01-18 16:10:58 +0400

    changed books quantity

--- book-finder-project/js/FetchBooks.js
+++ book-finder-project/js/FetchBooks.js
@@ -1,5 +1,5 @@
 let getBook = async (searchQueryPart) => {
-    let response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchQueryPart}`);
+    let response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchQueryPart}&maxResults=40`);
     let myJson = await response.json();
     return myJson;
 }
