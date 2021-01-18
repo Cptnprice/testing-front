b41e1de7f647f18491fb096651a0794085d925a9
shalva <shalvachkuaselidze77@gmail.com>
2021-01-18 14:39:37 +0400

    test

--- book-finder-project/js/detail.js
+++ book-finder-project/js/detail.js
@@ -14,29 +14,31 @@ let getCurrentBook = async () => {
     return JSON.parse(localStorage.getItem("currentBook"));
 }
 
-loaderContainer.style.display = "flex";
-let book = await getCurrentBook();
-loaderContainer.style.display = "none";
-let bookDetailsInformation = document.createElement('div');
-let bookDescriptionContainer = document.createElement('div');
-bookDetailsInformation.classList.add("book-details-information");
-bookDescriptionContainer.classList.add("book-description");
-let commonFields = createCommonFields(book);
-let bookImage = document.createElement("img");
-bookImage.classList.add("book-detail-image");
-bookImage.src = book.volumeInfo.imageLinks.thumbnail;
-let numberOfPages = document.createElement('p');
-numberOfPages.innerHTML = `<span class="bold-span">Number of pages</span>: ${book.volumeInfo.pageCount}`;
-bookDetailsInformation.appendChild(bookImage);
-commonFields.forEach((field) => {
-    bookDetailsInformation.appendChild(field);
-});
-bookDetailsInformation.appendChild(numberOfPages);
-let bookDescriptionTitle = document.createElement('h3');
-bookDescriptionTitle.innerHTML = "Book Description";
-let bookDescription = document.createElement('p');
-bookDescription.innerHTML = book.volumeInfo.description ? book.volumeInfo.description : '';
-bookDescriptionContainer.appendChild(bookDescriptionTitle);
-bookDescriptionContainer.appendChild(bookDescription);
-bookInformation.appendChild(bookDetailsInformation);
-bookInformation.appendChild(bookDescriptionContainer);
+window.onload = async () => {
+    loaderContainer.style.display = "flex";
+    let book = await getCurrentBook();
+    loaderContainer.style.display = "none";
+    let bookDetailsInformation = document.createElement('div');
+    let bookDescriptionContainer = document.createElement('div');
+    bookDetailsInformation.classList.add("book-details-information");
+    bookDescriptionContainer.classList.add("book-description");
+    let commonFields = createCommonFields(book);
+    let bookImage = document.createElement("img");
+    bookImage.classList.add("book-detail-image");
+    bookImage.src = book.volumeInfo.imageLinks.thumbnail;
+    let numberOfPages = document.createElement('p');
+    numberOfPages.innerHTML = `<span class="bold-span">Number of pages</span>: ${book.volumeInfo.pageCount}`;
+    bookDetailsInformation.appendChild(bookImage);
+    commonFields.forEach((field) => {
+        bookDetailsInformation.appendChild(field);
+    });
+    bookDetailsInformation.appendChild(numberOfPages);
+    let bookDescriptionTitle = document.createElement('h3');
+    bookDescriptionTitle.innerHTML = "Book Description";
+    let bookDescription = document.createElement('p');
+    bookDescription.innerHTML = book.volumeInfo.description ? book.volumeInfo.description : '';
+    bookDescriptionContainer.appendChild(bookDescriptionTitle);
+    bookDescriptionContainer.appendChild(bookDescription);
+    bookInformation.appendChild(bookDetailsInformation);
+    bookInformation.appendChild(bookDescriptionContainer);
+}
