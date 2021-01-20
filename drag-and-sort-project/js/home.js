94cfb03131f2c3974f02c4fbe7309075d32cff46
shalva <shalvachkuaselidze77@gmail.com>
2021-01-20 23:46:05 +0400

    added second container for testing

--- drag-and-sort-project/js/home.js
+++ drag-and-sort-project/js/home.js
@@ -1,18 +1,29 @@
 let firstBox = document.getElementById("testing");
+let secondBox = document.getElementById("testing2");
+let boxes = [firstBox, secondBox];
 let previousX;
 let previousY;
 
 let testingFunction = (e1) => {
-    firstBox.style.top = firstBox.offsetTop + (e1.clientY - previousY) + "px";
-    firstBox.style.left = firstBox.offsetLeft + (e1.clientX - previousX) + "px";
+    box = e1.target;
+    box.style.top = box.offsetTop + (e1.clientY - previousY) + "px";
+    box.style.left = box.offsetLeft + (e1.clientX - previousX) + "px";
     previousX = e1.clientX;
     previousY = e1.clientY;
 }
 
-firstBox.addEventListener("mousedown", (e) => {
-    previousX = e.clientX;
-    previousY = e.clientY;
-    firstBox.addEventListener("mousemove", testingFunction);
+boxes.forEach((box) => {
+    box.addEventListener("mousedown", (e) => {
+        previousX = e.clientX;
+        previousY = e.clientY;
+        box.addEventListener("mousemove", testingFunction);
+    })
+});
+
+// firstBox.addEventListener("mousedown", (e) => {
+//     previousX = e.clientX;
+//     previousY = e.clientY;
+//     firstBox.addEventListener("mousemove", testingFunction);
     // console.log("screenX : ", e.clientX, "screenY : ", e.clientY);
     // if ((e.clientX >= firstBox.offsetLeft - 100 && e.clientX <= firstBox.offsetLeft + 100) && (e.clientY >= firstBox.offsetTop - 100 && e.clientY <= firstBox.offsetTop + 100)) {
     //     console.log("test9");
@@ -22,8 +33,11 @@ firstBox.addEventListener("mousedown", (e) => {
     //     console.log("x coordinate : ", e.clientX >= firstBox.offsetLeft && e.clientX <= firstBox.offsetLeft + 200, "offsetLeft : ", firstBox.offsetLeft);
     //     console.log("y coordinate : ", e.clientY >= firstBox.offsetTop && e.clientY <= firstBox.offsetTop + 200, "offsetTop : ", firstBox.offsetTop);
     // }
-});
+// });
 
-firstBox.addEventListener("mouseup", () => {
-    firstBox.removeEventListener("mousemove", testingFunction);
-})
+
+boxes.forEach((box) => {
+    box.addEventListener("mouseup", () => {
+        box.removeEventListener("mousemove", testingFunction);
+    });
+});
