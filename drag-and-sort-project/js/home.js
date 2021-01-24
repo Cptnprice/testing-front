68530b7c03b1d98132407e7c9828b0f52f6cafb3
unknown <shalvachkuaselidze77@gmail.com>
2021-01-24 17:50:29 +0400

    moving in both directions

--- drag-and-sort-project/js/home.js
+++ drag-and-sort-project/js/home.js
@@ -18,7 +18,7 @@ let moveBox = (e1) => {
     currentBoxIndex = boxes.indexOf(currentBox);
     currentBox.style.top = currentBox.offsetTop + (e1.clientY - previousY) + "px";
     currentBox.style.left = currentBox.offsetLeft + (e1.clientX - previousX) + "px";
-    let boxes1 = boxes.filter((box) => box.offsetLeft < temp1);
+    let boxes1 = boxes;
     for (let i = 0; i < boxes1.length; i++) {
         let box = boxes1[i];
             if ((box.offsetLeft + 100 >= e1.clientX && box.offsetLeft <= temp1) && (e1.clientY >= box.offsetTop - 100 && e1.clientY <= box.offsetTop + 100)) {
@@ -26,6 +26,13 @@ let moveBox = (e1) => {
                         box.classList.add("move");
                         box.style.left = box.offsetLeft + 200 + "px";
                         boxes2.push(box);
+                }
+            }
+            else if ((box.offsetLeft <= e1.clientX && box.offsetLeft > temp1) && (e1.clientY >= box.offsetTop - 100 && e1.clientY <= box.offsetTop + 100)) {
+                if (!boxes2.includes(box)) { 
+                    box.classList.add("move");
+                    box.style.left = box.offsetLeft - 200 + "px";
+                    boxes2.push(box);
                 }
             }
     }
