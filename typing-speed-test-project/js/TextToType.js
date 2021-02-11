36db841fafd5b80d1f0b9b1106e455c9415093c1
shalva <shalvachkuaselidze77@gmail.com>
2021-02-11 15:03:09 +0400

    added some comments

--- typing-speed-test-project/js/TextToType.js
+++ typing-speed-test-project/js/TextToType.js
@@ -1,12 +1,13 @@
 const getText = async () => {
     let textToType = "";
-    let temp;
+    let tempText; // temporarily fetched text variable, if regex check is successful, textToType will be asigned to it
     while (!textToType) {
         const response = await fetch("https://en.wikipedia.org/api/rest_v1/page/random/summary");
         const myJson = await response.json();
-        temp = myJson["extract"];
-        if (temp.match(/["a-zA-Z0-9!?\-,\.\&\$\%\@\#\^\*\(\)\_\+\=\ "]+/g)[0] == temp) {
-            textToType = temp;
+        tempText = myJson["extract"];
+        // regex to check if text to type contains only english character and also some special characters
+        if (tempText.match(/["a-zA-Z0-9!?\-,\.\&\$\%\@\#\^\*\(\)\_\+\=\ "]+/g)[0] == tempText) {
+            textToType = tempText;
         }
     }
     return textToType;
--- typing-speed-test-project/js/home.js
+++ typing-speed-test-project/js/home.js
@@ -5,14 +5,14 @@ import { wordsPerMinuteTimerId, wordsPerMinute, wordsPerMinuteTimer, increaseTot
 let textToType;
 let words;
 let typedWords;
-let i = 0;
-let testing;
-let testing2 = 0;
-let testing3 = 0;
+let currentWordIndex = 0;
+let currentWordLength;
+let correctCharactersQuantity = 0;
+let totalCharactersQuantity = 0;
 let incorrectTyped = false;
-let first;
-let typedText = document.getElementById("typed-text");;
-let temp;
+let currentWord;
+let typedText = document.getElementById("typed-text");
+let typedWord;
 
 let loader = document.getElementById("loading-text");
 
@@ -22,75 +22,84 @@ function measure(e) {
         wordsPerMinuteTimer();
         setStopwatchStarted();
     }
-    testing = words[i].length;
-    temp = document.getElementById(i);
-    if (testing2 < testing) {
-        if (e.data == words[i][testing2]) {
-            if (typedText.value == words[i].substring(0, testing2+1)) {
-                increaseTotalTypedCharacters();
-                temp.innerHTML = temp.innerHTML.substring(0, temp.innerHTML.includes("</span>") ? (temp.innerHTML.lastIndexOf("</span>")+7) : -1) + `<span class="typed-character">${words[i][testing2]}</span>` + words[i].substring(testing2+1);
+    currentWordLength = words[currentWordIndex].length;
+    typedWord = document.getElementById(currentWordIndex);
+    // word is not fully typed correctly yet
+    if (correctCharactersQuantity < currentWordLength) {
+        // typed character is correct
+        if (e.data == words[currentWordIndex][correctCharactersQuantity]) {
+            // make sure that not only typed characters is correct, but also previous characters as well
+            if (typedText.value == words[currentWordIndex].substring(0, correctCharactersQuantity+1)) {
+                increaseTotalTypedCharacters(); // increase total typed characters amount for whole text
+                typedWord.innerHTML = typedWord.innerHTML.substring(0, typedWord.innerHTML.includes("</span>") ? (typedWord.innerHTML.lastIndexOf("</span>")+7) : -1) + `<span class="typed-character">${words[currentWordIndex][correctCharactersQuantity]}</span>` + words[currentWordIndex].substring(correctCharactersQuantity+1);
                 incorrectTyped = false;
-                testing2++;
-                testing3++;
+                correctCharactersQuantity++;
+                totalCharactersQuantity++;
             }
         }
         else {
-            if (!e.data && typedText.value.length < words[i].length) {
-                let spanElements = temp.querySelectorAll("span");
-                let lastIndexOfOpenSpan = temp.innerHTML.lastIndexOf("<span ");
-                let lastIndexOfCloseSpan = temp.innerHTML.lastIndexOf("</span>");
-                temp.innerHTML = temp.innerHTML.substring(0, lastIndexOfOpenSpan) + spanElements[spanElements.length - 1].innerHTML + temp.innerHTML.substring(lastIndexOfCloseSpan+7, temp.innerHTML.length);
-                if (testing2 >= 1 && !incorrectTyped) {
-                    testing2--;
+            // backspace(deleting character)
+            if (!e.data && typedText.value.length < words[currentWordIndex].length) {
+                let spanElements = typedWord.querySelectorAll("span");
+                let lastIndexOfOpenSpan = typedWord.innerHTML.lastIndexOf("<span ");
+                let lastIndexOfCloseSpan = typedWord.innerHTML.lastIndexOf("</span>");
+                typedWord.innerHTML = typedWord.innerHTML.substring(0, lastIndexOfOpenSpan) + spanElements[spanElements.length - 1].innerHTML + typedWord.innerHTML.substring(lastIndexOfCloseSpan+7, typedWord.innerHTML.length);
+                if (correctCharactersQuantity >= 1 && !incorrectTyped) {
+                    correctCharactersQuantity--;
                 }
-                testing3--;
+                totalCharactersQuantity--;
             }
+            // typed character is incorrect
             else {
-                if (testing3 < testing) {
-                    temp.innerHTML = temp.innerHTML.substring(0, temp.innerHTML.includes("</span>") ? (temp.innerHTML.lastIndexOf("</span>")+7) : -1) + `<span class="incorrect-character">${words[i][testing3]}</span>` + words[i].substring(testing3+1);
+                if (totalCharactersQuantity < currentWordLength) {
+                    typedWord.innerHTML = typedWord.innerHTML.substring(0, typedWord.innerHTML.includes("</span>") ? (typedWord.innerHTML.lastIndexOf("</span>")+7) : -1) + `<span class="incorrect-character">${words[currentWordIndex][totalCharactersQuantity]}</span>` + words[currentWordIndex].substring(totalCharactersQuantity+1);
                     incorrectTyped = true;
-                    testing3++;
+                    totalCharactersQuantity++;
                 }
             }
         }
     }
+    // word is fully typed correctly
     else {
-        document.getElementById(i).classList.remove("underline-word");
+        document.getElementById(currentWordIndex).classList.remove("underline-word");
         typedWords++;
+        // whole text is typed
         if (typedWords == words.length) {
-            let result = document.getElementById("test");
+            let result = document.getElementById("result");
             result.classList.add("completed");
             result.innerHTML = `Your WPM is ${wordsPerMinute}. <a href=".">New one</a>`;
             clearTimeout(timerId);
             clearTimeout(wordsPerMinuteTimerId);
             typedText.removeEventListener("input", measure);
         }
+        // whole text is not fully typed yet
         else {
-            i++;
-            document.getElementById(i).classList.add("underline-word");
-            testing2 = 0;
-            testing3 = 0;
+            currentWordIndex++;
+            document.getElementById(currentWordIndex).classList.add("underline-word");
+            correctCharactersQuantity = 0;
+            totalCharactersQuantity = 0;
             typedText.value = "";
             incorrectTyped = false;
         }
     }
 }
 
+// call API to get text and then show it
 getText().then((result) => {
     textToType = result.substring(0, result.length - 1);
     let textToTypeTest = document.getElementById("text-to-type-test");
     words = textToType.split(" ");
-    words.forEach((word, i) => {
-        let temp = document.createElement("p");
-        temp.innerHTML = word;
-        temp.id = i;
-        textToTypeTest.appendChild(temp);
+    words.forEach((word, currentWordIndex) => {
+        let typedWord = document.createElement("p");
+        typedWord.innerHTML = word;
+        typedWord.id = currentWordIndex;
+        textToTypeTest.appendChild(typedWord);
     });
     loader.style.display = "none";
     typedWords = 0;
-    testing = words[i].length;
-    first = document.getElementById(i);
-    first.classList.add("underline-word");
+    currentWordLength = words[currentWordIndex].length;
+    currentWord = document.getElementById(currentWordIndex);
+    currentWord.classList.add("underline-word");
 
     typedText.addEventListener("input", measure)
 });
